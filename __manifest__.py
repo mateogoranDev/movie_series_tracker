7f4d60e01a7a8567b4f881a68025cc14d7b71543
Mateogoran <mateogoran7bc@gmail.com>
2026-06-30 12:58:11 +0200

    Añadida URL de prueba y actualizada versión para la tienda de Odoo

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Cine & Series Tracker',
-    'version': '18.0.1.0.0',
+    'version': '18.0.1.0.1',
     'category': 'Tools',
     'summary': 'Gestiona tus películas y series favoritas, puntuaciones y catálogo personal con integración TMDB.',
     'author': 'mateogoranDev',
@@ -9,7 +9,8 @@
         'security/ir.model.access.csv',
         'views/movie_views.xml',
     ],
-    'images': ['static/description/banner.png'],  # Opcional: ayuda a las tiendas a previsualizar tu módulo
+    'images': ['static/description/banner.png'],
+    'live_test_url': 'http://51.170.44.49:8069',
     'installable': True,
     'application': True,
     'license': 'LGPL-3',
